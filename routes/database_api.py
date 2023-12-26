f89663568d90dbccd6f02b4133ea3a065f92ee02
윤규희 <ojinga0519@naver.com>
2023-12-26 16:12:03 +0900

    S3-74

--- routes/database_api.py
+++ routes/database_api.py
@@ -95,25 +95,49 @@ def get_donation():
     try:
         with connect(**connectionString) as con:
             cursor = con.cursor()
-            sql = "SELECT * FROM coupon order by usepoint"
+            sql = "SELECT * FROM mileage_category where category='donation' order by usepoint"
             cursor.execute(sql)
-            coupon_list = cursor.fetchall()
+            donation_list = cursor.fetchall()
             con.commit()
-            return coupon_list
+            return donation_list
     except Exception as e:
         print(e)
 
 
-def use_coupon(user_email, ):
+def use_coupon(user_email, coupon_id):
     try:
         with connect(**connectionString) as con:
             cursor = con.cursor()
-            sql = "SELECT * FROM coupon order by usepoint"
-            cursor.execute(sql)
-            coupon_list = cursor.fetchall()
+            sql = "SELECT usepoint FROM mileage_category where id = %s"
+            cursor.execute(sql, (coupon_id,))
+            result = cursor.fetchall()
+            use_point = result[0]['usepoint']
             con.commit()
-            return coupon_list
+        with connect(**connectionString) as con:
+            cursor = con.cursor()
+            mileage_before_sql = "SELECT mileage FROM user WHERE email = %s"
+            cursor.execute(mileage_before_sql, (user_email,))
+            result = cursor.fetchall()
+            con.commit()
+            mileage_before = result[0]['mileage']
+            mileage_after = mileage_before - use_point
+
+            sql = "UPDATE user SET mileage = mileage - %s WHERE email = %s"
+            cursor.execute(sql, (use_point, user_email))
+            result = cursor.fetchone()
+            con.commit()
+            affected_rows = cursor.rowcount
+            if affected_rows > 0:
+                # additional_sql = "SELECT mileage FROM user where email = %s"
+                # cursor.execute(additional_sql, (user_email,))
+                # use_point = cursor.fetchall()[0]['mileage']
+                # con.commit()
+                mileage_tracking_sql = "INSERT INTO milege_tracking (user_email, mileage_category_id, before_mileage, after_mileage) VALUES (%s, %s, %s, %s)"
+                cursor.execute(mileage_tracking_sql, (user_email, coupon_id, mileage_before, mileage_after))
+                con.commit()
+                return mileage_after
     except Exception as e:
         print(e)
+        return 500
 
 
