cf79dbc30deee6360deef1c116a7e794bae052c9
윤규희 <ojinga0519@naver.com>
2024-01-18 17:27:10 +0900

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -15,4 +15,4 @@ chown -R ubuntu /home/ubuntu/ssg_backend
 sudo chown -R ubuntu:ubuntu /home/ubuntu/ssg_backend
 
 echo ">>> start server ---------------------"
-gunicorn --bind 0.0.0.0:5000 --timeout 90 --log-level=debug "app:create_app()" >> "$LOGFILE" 2>&1 &
+gunicorn --bind 0.0.0.0:5000 --timeout 90 "app:create_app()" >> "$LOGFILE" 2>&1 &
